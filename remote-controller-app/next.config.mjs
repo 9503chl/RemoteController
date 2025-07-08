530f6993e0416d0fbb5ba4162437b9138cb65c59
최유섭 <80661998+9503chl@users.noreply.github.com>
2025-07-08 20:57:37 +0900

    25/07/08 -3
    수정

--- remote-controller-app/next.config.mjs
+++ remote-controller-app/next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:8080/:path*', // 백엔드 서버로 프록시
-            },
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig; 
